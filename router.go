4929c4eabe87c17bff218eb8ef63f9a6b2fa6b6b
ys902 <yogasutrisna@gmail.com>
2020-08-24 15:11:02 +0700

    log paths

--- router.go
+++ router.go
@@ -4,6 +4,7 @@ import (
 	"api/db"
 	"api/controllers"
 	"api/config"
+	"log"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -18,6 +19,8 @@ func router() *echo.Echo {
 		AllowOrigins: config.CORSAllowOrigin,
 	}))
 
+	e.Use(printUrlMiddleware)
+
 	// Post
 	e.GET("/posts/:field/:key", controllers.GetCmsPostList).Name = "GetCmsPostList"
 	e.GET("/posts/:key", controllers.GetCmsPostData).Name = "GetCmsPost"
@@ -29,5 +32,13 @@ func router() *echo.Echo {
 	e.POST("/register", controllers.Register).Name = "Register"
 	e.POST("/verifyemail", controllers.VerifyEmail).Name = "VerifyEmail"
 	e.POST("/verifyotp", controllers.VerifyOtp).Name = "VerifyOtp"
+	e.POST("/login", controllers.VerifyOtp).Name = "Login"
 	return e
+}
+
+func printUrlMiddleware (next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		log.Println(c.Request().URL)
+		return next(c)
+	}
 }
