12735d6d020ba4cb67c152dc283d66d7cb67ae18
ys902 <yogasutrisna@gmail.com>
2020-08-27 08:44:58 +0700

    change method for verify emai

--- router.go
+++ router.go
@@ -30,7 +30,7 @@ func router() *echo.Echo {
 
 	// Session
 	e.POST("/register", controllers.Register).Name = "Register"
-	e.POST("/verifyemail", controllers.VerifyEmail).Name = "VerifyEmail"
+	e.GET("/verifyemail", controllers.VerifyEmail).Name = "VerifyEmail"
 	e.POST("/verifyotp", controllers.VerifyOtp).Name = "VerifyOtp"
 	e.POST("/login", controllers.Login).Name = "Login"
 	e.POST("/resendverification", controllers.ResendVerification).Name = "ResendVerification"
