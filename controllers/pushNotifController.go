a1c0437aa0c2fca8643ada06dfc96eee437d12ef
yosua <yosua@docotel.com>
2021-01-07 14:29:02 +0700

    push notif success

--- controllers/pushNotifController.go
+++ controllers/pushNotifController.go
@@ -1,42 +1,37 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
 
-	"github.com/douglasmakey/go-fcm"
+	"github.com/maddevsio/fcm"
 )
 
 func CreateNotification(token string) {
-	// init client
-	// client := fcm.NewClient("AAAASUHZQg4:APA91bG165ItNXI_35P27hHURe3PHXhFqxtOMo4XpsIdSgsK2_No--tb9CXGIplhP1DZfLlsyLAB2LKct16wbpyInBKIlrBnepzwoVaAjyoGMYDe4KArzQR20R3T7hwIkzIyKwXICLtp")
-	client := fcm.NewClient("AAAACgKWnuY:APA91bEdhBJmGR5mYx9Lyd8jimPSh8bAz65ao6cCmmA3-O1vJBIML7a6-IyQ0b9giER2-EYpWBWriJdODPdSMmaAZsWCcxdgsnx_lpSACx5HCKMug8wDs0XrrDbzsbiaVo6rl3_ui84q")
+	data := map[string]string{
+		"msg": "Hello World1",
+		"sum": "Happy Day",
+	}
 
-	// You can use your HTTPClient
-	//client.SetHTTPClient(client)
+	serverKey := "AAAACgKWnuY:APA91bEdhBJmGR5mYx9Lyd8jimPSh8bAz65ao6cCmmA3-O1vJBIML7a6-IyQ0b9giER2-EYpWBWriJdODPdSMmaAZsWCcxdgsnx_lpSACx5HCKMug8wDs0XrrDbzsbiaVo6rl3_ui84q"
 
-	data := map[string]interface{}{
-		"message": "From Go-FCM",
-		"details": map[string]string{
-			"name":  "Name",
-			"user":  "Admin",
-			"thing": "none",
+	c := fcm.NewFCM(serverKey)
+	response, err := c.Send(fcm.Message{
+		Data:             data,
+		RegistrationIDs:  []string{token},
+		ContentAvailable: true,
+		Priority:         fcm.PriorityHigh,
+		Notification: fcm.Notification{
+			Title: "Hello",
+			Body:  "World",
 		},
-	}
-
-	// You can use PushMultiple or PushSingle
-	// client.PushMultiple([]string{"token 1", "token 2"}, data)
-	client.PushSingle(token, data)
-
-	// registrationIds remove and return map of invalid tokens
-	badRegistrations := client.CleanRegistrationIds()
-	log.Println(badRegistrations)
-
-	status, err := client.Send()
+	})
 	if err != nil {
-		log.Println("---------------------------")
-		log.Println(err)
-		log.Println("---------------------------")
+		log.Fatal(err)
 	}
-
-	log.Println(status.Results)
+	fmt.Println("Status Code   :", response.StatusCode)
+	fmt.Println("Success       :", response.Success)
+	fmt.Println("Fail          :", response.Fail)
+	fmt.Println("Canonical_ids :", response.CanonicalIDs)
+	fmt.Println("Topic MsgId   :", response.MsgID)
 }
